84aa75e81499198730d09927d65e233a999eaec4
Shrivu Shankar <shrivu1122@gmail.com>
2023-04-27 20:42:46 -0700

    app

--- app/web.py
+++ app/web.py
@@ -1,8 +1,12 @@
-import flask
+from flask import Flask, render_template
 
-app = flask.Flask(__name__)
+app = Flask(__name__, static_folder="build/static", template_folder="build")
 
 
 @app.route("/")
 def index():
-    return flask.jsonify({"status": "OK"})
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(port=5000)
